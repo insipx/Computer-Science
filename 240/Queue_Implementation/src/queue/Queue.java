8da571a7aafec5bea17a6ce48ff3c3a1082be053
Andrew Plaza <andrew.plaza15@gmail.com>
2016-02-19 15:54:18 -0800

    finished queue implementation

--- 240/Queue_Implementation/src/queue/Queue.java
+++ 240/Queue_Implementation/src/queue/Queue.java
@@ -21,18 +21,27 @@ public class Queue<T> implements QueueInterface<T> {
 	@Override
 	public void enqueue(T val) {
 		if(size == 0){
-			//temp node
+			//temp node just for the first value
 			firstVal = new Node<T>();
+			//store the first value the user gives us
 			firstVal.value = val;
+			//since it's the first, it gets to be the front and end
 			front = firstVal;
 			end = firstVal;
+			//but the end will point to nothing
 			end.ref = null;
+			//and we have to keep track of the size
 			size++;
 		}else{
+			//make anotehr temp node
 			Node<T> qNode = new Node<T>();
+			//the end will point to this
 			end.ref = qNode;
+			//our temp node will now point to nothing
 			qNode.ref = null;
+			//it becomes the end
 			end = qNode;
+			//make sure to get the val and increment size
 			end.value = val;
 			size++;
 		}
@@ -40,33 +49,37 @@ public class Queue<T> implements QueueInterface<T> {
 
 	@Override
 	public T dequeue() {
-		if(size <= 0){
-			return null;
-		}else{
-			//the temp node
+		if(size > 0 && front != null){
 			Node<T> qNode = new Node<T>();
+			//store the current front in a temp
 			qNode = front;
+			//make the next node our new front, the old one will get taken care of by garbage collectiong
 			front = front.ref;
+			//don't forget to decrement
+			size--;
 			return qNode.value;
-
+		}else{
+			//if there is nothing left
+			System.out.println("nothing left :'(");
+			return null;
 		}
 	}
 
 	@Override
 	public void dumpqueue() {
-		//create two temporary nodes, and start them off at the current node. 
+		//create a temporary node, and start them off at the front 
 		//we don't want dumpstack to change what's in the stack
 		Node<T> temp = new Node<T>();
-		Node<T> tempCurrent = new Node<T>();
-		tempCurrent = front;
-		//iterate through the stack, printing every element O(n)
+		temp = front;
+		//iterate through the queue, printing every element O(n)
+		System.out.println("---------- DumpQueue------------");
 		for(int i = 0; i < size; i++){
-			temp = tempCurrent;
+			
 			System.out.println(temp.value);
 			temp = temp.ref;
-			tempCurrent = tempCurrent.ref;
-		}
 
-		
+		}
+		System.out.println("---------- DumpQueue------------");
+
 	}
 }
