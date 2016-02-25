594e113778c9a1256ca9b9c0bb8e48b546c4730a
sbatzel <stbatzel@gmail.com>
2016-02-25 14:00:03 -0500

    Basically just commented the stuffing out of everything and made some suggestions. Few changes- I did make one and it didn't do diddly squat.

--- 250/UUIAccumulatorC/UUI.c
+++ 250/UUIAccumulatorC/UUI.c
@@ -4,7 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Alright I'm up to like 750 mb of memory used.
 
+//Pseudo-constructor that just sends the program back exactly what it
+//receives as a parameter.
 char *newUUI(char* str){
   return str;
 }
@@ -17,13 +20,14 @@ void printUUIf(char *str){
   printf("%s", str);
 }
 //my n00b error message function
+//Basically just a primitive exception-type thing.
 void die(const char *message)
 {
   if(message){
     printf("ERROR: %s\n", message);
   }else{
   }
-  exit(1);
+  exit(1); //The "nervous breakdown" statement.
 }
 int getSize(char *str){
   char c;
@@ -97,7 +101,7 @@ int i;
 for(i=0; i < resultSize; i++){
     if(result[i] >= '0' && result[i] <= '9')
     {
-      
+      //There's actually a isDigit() function.
     }else{
       //if it's not a digit remove it and make the string smaller
      //don't do nothing yet I will develop this later
@@ -114,6 +118,7 @@ for(i=0; i < resultSize; i++){
 //so our best bet is to use fgetc
 //dynamically allocates memory so we only use exactly what we need
 //I KNOW THIS METHOD WORkS FOR FACT DON'T TOUCH IT
+//Does it really, though?
 char * readUUI()
 {
   //allocate 64bytes for usage
@@ -155,6 +160,8 @@ char toChar(int i){ return i + '0'; }
 //because sizeof returns the size of the pointer -_-
 
 
+//I don't know if we need this if we just use strcmp. We shouldn't even need
+//the boolean typedef.
 int checkEquality(char *charA, char *charB){
 int i;  
 for(i = 0; i < getSize(charA); i++)
@@ -184,20 +191,30 @@ int NE(char *val0, char *val1){
   } else {
     return TRUE;
   }
-
 }
 
+//I handled NE like this, I just ix-nayed the oolean-bay and it works perfectly.
+//We might want to try that it'll also speed us up and we'll take up slightly
+//less space in memory.
+//int NE(char *one, char *two){
+//    return strcmp(one,two);
+//}
+//Yep okay I tried replacing the original with this, didn't work. Just kept adding more 0's to the string??? Yeah actually does the same thing the way it is right now.
+
+
+//What's the idea behind this sub?
 char * addTen(char *charArr, int j, int tmp)
 {
-  if(toInt(charArr[j-1] + 1) >= 10)
+  if(toInt(charArr[j-1] + 1) >= 10) //If the last digit is greater than 1 digit
   {
-    charArr[j] = toChar(tmp % 10);
-    tmp = toInt(charArr[j-1] + 1);
-    j--;
-    addTen(charArr, j, tmp);
-  } else {
-    charArr[j-1] = toChar(toInt(charArr[j-1] + 1));
-    charArr[j] = toChar(tmp % 10);
+    charArr[j] = toChar(tmp % 10); //Take the 1's place,
+    tmp = toInt(charArr[j-1] + 1); //Carry the 10's place.
+    j--; //Go to the next index of the array.
+    addTen(charArr, j, tmp); //Continue the operation. Hang on, what's tmp?
+  } else { //Else if it's just 1 digit,
+    charArr[j-1] = toChar(toInt(charArr[j-1] + 1)); // Add one? Why?
+    //Okay, so the last index is the character of the integer of the itself, plus 1?
+    charArr[j] = toChar(tmp % 10); //Take the 1's place. This might be marginally overcomplicated.
   } 
   return charArr;
 }
@@ -205,6 +222,7 @@ char * addTen(char *charArr, int j, int tmp)
 char * endCalc(char *charArr)
 {
   //look at alll thooooose zeroooooos :P
+  //what does this even do?
   char *endCalc = "";
   int i = 0;
   if(charArr[0] == '0'){
@@ -226,11 +244,13 @@ char * endCalc(char *charArr)
 }
 
 
+//This is where the fun part is...
 char * sum(char *val0, char *val1)
 {
   int size;
   
   // -1 because nullbyte 
+  //Okay, here we're just getting the sizes of the variables we need?
   int val0Size = getSize(val0);
   int val1Size = getSize(val1);
   
@@ -238,6 +258,8 @@ char * sum(char *val0, char *val1)
   //printf("%s%d%s", " || This is val0 ", val0Size, " || \n"); 
   //printf("%s%d%s", " || This is val1 ", val1Size, " || \n"); 
 
+  //Makin' sure we've got a big enough array for the whole number.
+  //Don't forget we need it 1 bigger.
   if (val0Size > val1Size){
     size = val0Size;
   }
@@ -250,7 +272,8 @@ char * sum(char *val0, char *val1)
   printf("%s%d%s", "This is the size ", size, "\n");
   printf("%s%d%s", "This is the val0Size ", val0Size, "\n");
 
-
+  //I'm sitting here watching my memory usage slowly climb... 
+  //Something on my Mint system has a huge leak and I think this might be it.
   //account for an extra zero at the beginning
   char *numArr = makeArray(val0, size);
   printf("%s%s%s", "This is numARR AFTER: ", numArr, "\n");
@@ -276,6 +299,7 @@ char * sum(char *val0, char *val1)
     }
   }
   //let's leave endCalc out of this for now
+  //Honestly, what does this subroutine even do?
   //val0 = endCalc(numArr);
   val0 = numArr;  
   printf("%s%s%s", "this is val0", val0, "\n"); 
