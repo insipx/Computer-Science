9c47b1f1565e84d7fc5235f8c0a7876a76619cb2
sbatzel <stbatzel@gmail.com>
2016-02-27 14:45:26 -0500

    Made some progress I think. Really doesn't work.

--- 240/Binary_Search_Tree/BinarySearchTreeInterface.java
+++ 240/Binary_Search_Tree/BinarySearchTreeInterface.java
@@ -1,6 +1,6 @@
 public interface BinarySearchTreeInterface{
 	void insert(Node node, int element);
 	void delete(Node node, int element);
-	void dumptree(Node node, int element);
+	void dumptree(Node node);
 	void pass(String message);
 }
--- 240/Binary_Search_Tree/Node.java
+++ 240/Binary_Search_Tree/Node.java
@@ -2,8 +2,10 @@ public class Node{
 	int value;
 	Node left;
 	Node right;
+	Node parent;
 
 	public Node(){
+		this.parent = null;
 		this.left = null;
 		this.right = null;
 	}
