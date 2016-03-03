847605d83571533efe1346991b1c6265d51be3d0
Andrew Plaza <andrew.plaza15@gmail.com>
2016-03-02 23:35:01 -0500

    untitled

---
+++ Help/untitled/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Created by insidious on 3/2/16.
+ */
+public class Main {
+    public static void main(String [] args){
+
+
+        for(int i = 1; i<=3; i++){
+       
+          for(int j = 1; j <=5; j++){
+              System.out.println("Hello, this is i " + i);
+              System.out.println("hello, this is j " + j);
+          }
+
+        }
+
+
+    }
+
+
+}
