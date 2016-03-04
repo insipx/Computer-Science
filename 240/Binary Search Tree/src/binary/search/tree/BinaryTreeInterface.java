98a08526812a5a654a553761ccc1d90999da1049
sbatzel <stbatzel@gmail.com>
2016-03-04 10:40:34 -0500

    I AM VICTORIOUS

--- 240/Binary Search Tree/src/binary/search/tree/BinaryTreeInterface.java
+++ 240/Binary Search Tree/src/binary/search/tree/BinaryTreeInterface.java
@@ -1,8 +1,7 @@
 package binary.search.tree;
 
 public interface BinaryTreeInterface {
-    void insert(Node node, int value);
-    void delete(Node node, int value);
-    void dumptree(Node node);
-    void pass(String msg);
+    void insert(TreeNode node, int value);
+    void delete(TreeNode node, int value);
+    void dumptree(TreeNode node);
 }
---
+++ 240/Binary Search Tree/src/binary/search/tree/TreeNode.java
@@ -0,0 +1,12 @@
+package binary.search.tree;
+
+public class TreeNode {
+    TreeNode right;
+    TreeNode left;
+    int value;
+    
+    public TreeNode(){
+        this.right = null;
+        this.left = null;
+    }
+}
