e53a0d70076869bb7b4abe7d4dee2fed80b95f6f
Andrew Plaza <andrew.plaza15@gmail.com>
2016-03-12 18:59:53 -0500

    maintenence

---
+++ Help/Examples/src/Main.java
@@ -0,0 +1,32 @@
+/**
+ * Created by insidious on 3/10/16.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+       int randomInt = 32;
+
+        System.out.print("Hello World \n");
+       System.out.print("Hello Universe");
+
+        int result = add(2 , 4);
+        String x = "Hello";
+        int a = 20;
+
+    }
+
+    public static int add(int a, int b){
+
+        return a + b;
+
+
+    }
+    public static int subtract(int a, int b){
+
+        return a - b;
+    }
+
+
+}
