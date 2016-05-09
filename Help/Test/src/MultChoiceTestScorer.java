039f16b72376cab352381ed74a9670cda953dc0a
Andrew Plaza <andrew.plaza15@gmail.com>
2016-05-09 17:56:45 -0400

    help

---
+++ Help/Test/src/MultChoiceTestScorer.java
@@ -0,0 +1,172 @@
+import java.util.Arrays;
+
+/* An instance of this class is for the purpose of scoring a multiple choice
+** test taken by a group of students.
+**
+** The test's answer key is established via a parameter passed to the
+** constructor.
+**
+** The scoreTest() method scores one student's test, given the student's
+** answers (via a parameter).
+**
+** Among the questions that can be answered (via calls to observers) are these:
+**
+**   --How many students' tests have been scored already?
+**   --Among the tests already scored, on how many was problem k answered
+**     correctly?
+**   --What raw score was achieved on the k-th test scored?
+**   --What is the average of the raw scores of the tests already scored?
+**   --What is the standard deviation of the raw scores of the tests already
+**     scored?
+**
+** Author: R. McCloskey and < student's name!!! >
+** Solution to Prog. Assg. #7, CMPS 134 Spring 2016
+** Date: May 2016
+** Aided by: ...
+** Known flaws: ...
+** 
+*/
+public class MultChoiceTestScorer {
+
+   // class constant
+   // --------------
+
+   // An approximation to the # of students expected to take the test.
+   private final int NUM_STUDENTS_DEFAULT = 20;
+
+   // instance constant
+   // -----------------
+
+   // ANSWER_KEY.charAt(k) is correct answer for problem #k
+   private final String ANSWER_KEY;
+
+
+   //raw scores :    index = test, problems answered correctly
+   //correctCount:   index=problem, tests
+   // instance variables
+   // ------------------
+   private int numTestsScored; // # of tests already scored
+   private int[] rawScores;    // rawScores[k] = # problems answered correctly
+                               // on the k-th test scored
+   private int[] correctCount; // correctCount[k] is # of already-scored tests
+                               // in which problem k was answered correctly
+
+   // constructor
+   // -----------
+
+   /* Initializes a test scorer object that can score a test having as its
+   ** answer key the String provided via the parameter.
+   */
+   public MultChoiceTestScorer(String correctAnswers)
+   {
+      ANSWER_KEY = correctAnswers;
+      final int N = correctAnswers.length();
+      correctCount = new int[N];
+      numTestsScored = 0;
+      rawScores = new int[NUM_STUDENTS_DEFAULT];
+   }
+
+
+   // observers
+   // ---------
+
+   /* Returns the answer key of the test.
+   */
+   public String answerKey() { return ANSWER_KEY; }
+
+
+   /* Returns the # of problems on the test.
+   */
+   public int numProblems() { return correctCount.length; }
+
+
+   /* Returns the number of tests that have been scored already.
+   */
+   public int numTestsScored() { return numTestsScored; }
+
+
+   /* Returns the raw score of the k-th test scored (counting from zero)
+   ** pre-condition: 0 <= k < numTestsScored()
+   */
+   public int rawScore(int k) { return rawScores[k]; }
+
+
+   /* Returns the average of the raw scores of the already-scored tests.
+   ** pre-condition: numTestsScored() > 0
+   */
+   public double averageRawScore()
+   {
+      int sum = 0;
+      for(int i = 0; i < rawScores.length;i++){
+         sum+=rawScores[i];
+      }
+      return sum/numTestsScored;
+   }
+
+
+   /* Returns the standard deviation of the raw scores of the already-scored
+   ** tests.
+   ** pre-condition: numTestsScored() > 0
+   */
+   public double stdDevOfRawScores() {
+      double average = averageRawScore();
+      int[] rawScoresCpy = rawScores;
+
+      for (int i = 0; i < rawScoresCpy.length; i++) {
+         rawScoresCpy[i] = rawScoresCpy[i] - (int) average;
+         rawScoresCpy[i] = rawScoresCpy[i] * rawScoresCpy[i];
+      }
+      int sum = 0;
+      for (int i = 0; i < rawScoresCpy.length; i++) {
+         sum += rawScoresCpy[i];
+      }
+      double average2 = sum / rawScoresCpy.length;
+      return Math.sqrt(average2);
+   }
+
+
+   /* Returns the number of already-scored tests in which problem #k 
+   ** was answered correctly.  (Problems are numbered starting at zero.)
+   ** pre-condition: 0 <= k < numProblems()
+   */
+   public int numCorrect(int k) { return correctCount[k]; }
+
+
+   // mutator
+   // -------
+
+   /* Given, via the parameter, the answers on a student's test paper,
+   ** scores that test (and records information regarding the raw score
+   ** and which problems were answered correctly).
+   ** pre-condition: stuAnswers.length == numProblems()  &&
+   **                numTestsScored() < maxNumberOfStudents()
+   */
+   public void scoreTest(String stuAnswers)
+   {
+      // In case the rawScores[] array is "full", double its length
+      if (numTestsScored == rawScores.length) {
+         rawScores = Arrays.copyOf(rawScores, 2 * rawScores.length);
+      }
+         int numCrct = 0;
+      for(int i = 0; i < ANSWER_KEY.length(); i++){
+         if(stuAnswers.charAt(i) == ANSWER_KEY.charAt(i)){
+            rawScores[i]++;
+         }
+         else{
+
+         }
+      }
+
+
+      //RWM: Keep the if-statement above and supply the rest of the
+      //     code needed here.  A character-by-character comparison between
+      //     ANSWER_KEY and stuAnswers (the formal parameter) is needed in
+      //     order to compute the raw score (which should be placed into
+      //     the rawScores[] array) and to appropriately update the elements
+      //     of the correctCount[] array.
+
+      // STUB!!
+
+   }
+
+}
---
+++ Help/Test/src/MultChoiceTestScorerTester.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+/* Java application having as its purpose to test the MultChoiceTestScorer
+** class.  The user is prompted to enter the test's answer key, after which
+** (s)he is prompted to enter each student's answers.  After each student's
+** answers are entered, the current state of the MultChoiceTestScorer object is
+** displayed, in effect, by calling the observers and printing the values
+** returned.
+*/
+
+public class MultChoiceTestScorerTester {
+
+   // Global variables referring to a Scanner and to the 
+   // MultipleChoiceTestScorer object under test.
+   private static Scanner input = new Scanner(System.in);
+   private static MultChoiceTestScorer scorer;
+
+   public static void main(String[] args)
+   {
+      String stuAnswerPrompt = "Enter student's answers (null string to quit):";
+
+      String answerKey = getStringFromUser("Enter answer key:");
+
+      scorer = new MultChoiceTestScorer(answerKey);
+
+      System.out.println("Answer key is " + scorer.answerKey()); 
+      System.out.println("# of problems on test is " + scorer.numProblems());
+
+      // Get 0-th student's answers.
+      String stuAnswers = getStringFromUser(stuAnswerPrompt);
+
+      while (stuAnswers.length() != 0)
+      {
+         scorer.scoreTest(stuAnswers);  // score the test
+
+         // Print info describing the state of the scorer object.
+         printNumTestsScored();
+         printRawScores();     // prints scores of all scored tests
+         printCorrectCounts(); // prints distribution of correct answers
+         printAverage();       // prints average raw score of scored tests
+         printStdDev();        // prints standard deviation of scored tests
+
+         // Remind user of answer key.
+         System.out.println("\nAnswer key is " + scorer.answerKey()); 
+
+         // Get next student's answers (empty string to quit).
+         stuAnswers = getStringFromUser(stuAnswerPrompt);
+      }
+      System.out.println("Goodbye.");
+   }
+
+
+   /* Prints the number of tests that have been scored.
+   */
+   private static void printNumTestsScored()
+   {
+         System.out.println("# of tests scored is " + scorer.numTestsScored());
+   }
+
+   /* Prints the raw scores of all scored tests.
+   */
+   private static void printRawScores()
+   {
+      for (int i=0; i != scorer.numTestsScored(); i = i+1) {
+         System.out.println("Student #" + i + " scored " + scorer.rawScore(i));
+      }
+   }
+
+   /* Prints the distribution of correct answers.
+   */
+   private static void printCorrectCounts()
+   {
+      for (int i=0; i != scorer.numProblems(); i = i+1) {
+         System.out.println("Problem #" + i + " answered correctly " + 
+                            scorer.numCorrect(i) + " times.");
+      }
+   }
+
+   /* Prints the average raw score of already-scored tests.
+   */
+   private static void printAverage()
+   {
+      System.out.println("Average raw score on scored tests is " +
+                         scorer.averageRawScore());
+   }
+
+   /* Prints the standard deviation of the raw scores of already-scored tests.
+   */
+   private static void printStdDev()
+   {
+      System.out.println("Standard Deviation of raw scores is " +
+                         scorer.stdDevOfRawScores());
+   }
+
+
+   /* Displays the specified prompt and returns the String entered in
+   ** response by the user.
+   */
+   private static String getStringFromUser(String prompt)
+   {
+      System.out.print(prompt);
+      return input.nextLine();
+   }
+
+}
