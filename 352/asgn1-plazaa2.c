7741e2d45f362f9384670efd03df0630bf0a424f
insidious <andrew.plaza15@gmail.com>
2016-08-29 19:55:44 -0400

    new programming asignment in 352

--- 352/asgn1-plazaa2.c
+++ 352/asgn1-plazaa2.c
@@ -1,26 +1,21 @@
-/* Program:	Hello World
-   author:	Andrew Plaza
-   Date:	Aug. 22, 2016
-   file name:   hello_world.c
-   compile:	gcc -o helloworld hello_world.c
-   run:		./helloworld
+/* Program: Programming Assignment 1, Linked List 
+ * author: Andrew Plaza
+ * Date: Aug. 29, 2016
+ * File Name: asgn1-plazaa2.c
+ * compile: gcc -o asgn1 asgn1-plazaa2.c
+ * run: ./asgn1
+ *
+ * This C program accepts player records from the keyboard, reads it into an ordered linked list, nd then prints the records in the list in the order they are stored.
+ */
+#include<stdio.h>
 
-   This C program accepts user's firstname and a positive
-   integer N; it then prints a personal greeting N times.
-*/
-#include <stdio.h>	// this is equivalent to Java's import
-
-int main()   {
-    char name[20];
-    int times;
-    int i = 0;
-    
-    printf("Please enter your firstname and a number:\n");
-    scanf("%s%d", name, &times);
-
-    // print greeting
-    for (i = 0; i < times; i++){
-        printf("Hello %s, your first program works!\n", name);
-    }
+struct player {
+  int userid;
+  char first[20];
+  char last[20];
+  int wins;
+  int losses;
+  int ties;
+  struct player*next;
 }
 
