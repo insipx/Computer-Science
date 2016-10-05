97ffad6b25d8871dde356760826b00df909c17ce
insidious <andrew.plaza15@gmail.com>
2016-10-04 22:05:05 -0400

    polishing it up

--- 352/asgn4-plazaa2.c
+++ 352/asgn4-plazaa2.c
@@ -18,8 +18,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-//typedefs, player is basically a node in the Linked List
-
 struct player {
   int userid;
   char first[20];
@@ -39,23 +37,25 @@ struct node {
 typedef struct node Node;
 typedef struct player Player;
 
-//method definitions
+//method definitions (same order as methods in file)
 
-int askinput(Node **head, int fd, int index);
+void readp(int fd, int index, Player *play);
+void writep(int fd, int index, Player *play);
 
+void print_nodes(Node **head);
+void print_players(int fd, Node **head);
+void printp(int fd, int index);
+
+void ask_input(int fd, int index, Node **head);
+
+Node* add(int fd, int index, Node **head);
 void insert(Node **head, Node *newNode);
 
-void printPlayers(Node **head, int fd);
-void printPlayer(int index, int fd);
+void update(int fd, Node **head);
+void query(int fd, Node **head);
 
-Node* add(Node **head, int fd, int index);
-void update(Node **head, int fd);
-void query(Node **head, int fd);
-
-void write_node(Player *node, int fd, int index);
 
 void kill(Node **head);
-void read_player(int fd, int index, Player *play);
 void die(const char *message);
 
 //LL = Linked List
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
   int index = 0, fd = 0; 
  
   if (argc <= 1){
-    fprintf(stderr, "Usage: './asgn4.out record_file.dat'\n");
+    fprintf(stderr, "Usage: './asgn4.out filename'\n");
     exit(1);
   }else{
     fd = open(argv[1], O_TRUNC|O_RDWR|O_CREAT, S_IRWXU);
@@ -73,32 +73,65 @@ int main(int argc, char *argv[]) {
   
   Node *head = NULL; //head node initialized in main
   
- 
-  index = askinput(&head, fd, index);
-
-  close(fd);
+  ask_input(fd, index, &head);
   
+  //end sequence
+  close(fd);
+  print_nodes(&head);
   kill(&head);
   printf("END\n");
 }
 
 
-void read_player(int fd, int index, Player *play){
+void readp(int fd, int index, Player *play){
   lseek(fd, index*sizeof(Player),0);
   if(read(fd, play, sizeof(Player)) == -1)
     die("[ERROR] read failed");
 }
-
-void write_node(Player *node, int fd, int index){
-  struct player play = *node;
+//Player *node, int fd, int index
+void writep(int fd, int index, Player *play){
+  struct player rec = *play;
   lseek(fd, index*sizeof(Player), 0);
-  if(write(fd, &play, sizeof(Player)) == -1)
+  if(write(fd, &rec, sizeof(Player)) == -1)
     die("[ERROR] write failed");
 }
 
+void print_nodes(Node **head){
+  Node *node = *head;
+  printf("The linked list: ");
+  while(node != NULL){
+    printf("(%d, %d)", node->userid, node->index);
+    node = node->next;
+  }
+  printf("\n");
+}
+
+void print_players(int fd, Node **head){
+  Node *temp = *head;
+  while(temp != NULL){
+    printp(fd, temp->index);
+    temp = temp->next;
+  }
+}
+
+void printp(int fd, int index){
+  struct player play;
+  readp(fd, index, &play);
+  
+  printf("%d, %s, %s, %d, %d, %d \n", play.userid, 
+                                      play.last, 
+                                      play.first, 
+                                      play.wins, 
+                                      play.losses, 
+                                      play.ties);
+  return;
+
+}
+
+
 //get first character in input,
 //go to method to do what user asks
-int askinput(Node **head, int fd, int index){
+void ask_input(int fd, int index, Node **head){
   
   Node *temp = *head;
   char c;
@@ -110,26 +143,26 @@ int askinput(Node **head, int fd, int index){
     c = getchar();
     
     if(c == '+'){
-      *head = add(&temp, fd, index);
+      *head = add(fd, index, &temp);
       index++;
     }
     else if(c == '*')
-      update(&temp, fd);
+      update(fd, &temp);
     else if (c == '?')
-      query(&temp, fd); 
+      query(fd, &temp); 
     else if (c == '\n');
       //ignore newline chars, reduced 'Error Invalid Input'
     else if (c == '#'){
       printf("TERMINATE\n");
-      printPlayers(&temp, fd);
-      return index;
+      print_players(fd, &temp);
+      return;
     }
   }while(c != '#');
 
-  return index;
+  return;
 }
 
-Node* add(Node **head, int fd, int index){
+Node* add(int fd, int index, Node **head){
   
   Node *temp = *head;
   Player *aNode = NULL;
@@ -142,7 +175,7 @@ Node* add(Node **head, int fd, int index){
   while(temp != NULL){
     if(temp->userid == aNode->userid){
       printf("ERROR - userid exists.\n");
-      printPlayer(temp->index, fd);
+      printp(fd, temp->index);
       printf("> ");
       return *head;
     }else temp = temp -> next;
@@ -154,11 +187,11 @@ Node* add(Node **head, int fd, int index){
   aNode->index = index; 
 
   //write node to binary file 
-  write_node(aNode, fd, index);
+  writep(fd, index, aNode);
      
   printf("%s", "ADD: ");
   
-  printPlayer(aNode->index, fd);
+  printp(fd, aNode->index);
   //allocate mem for LL node 
   //set new node to values of node just written to
   //persistant file 
@@ -177,104 +210,9 @@ Node* add(Node **head, int fd, int index){
 
   return *head;
 }
- 
-
-
-void update(Node **head, int fd){
-  int userid, wins, losses, ties;
-  Node *temp = *head;
-
-  //scan in the rest of the line
-  scanf("%d%d%d%d", &userid, &wins, &losses, &ties);
-   
-  //find data for playerid user entered, change info 
-  //for that node
-  while(temp != NULL){
-    if(temp->userid == userid) {
-      printf("%s", "BEFORE: ");
-      printPlayer(temp->index, fd);
-      Player *node = (Player *)malloc(sizeof(Player));
-      lseek(fd, temp->index*sizeof(Player), 0);
-      read_player(fd, temp->index, node);
-      node->userid = userid;
-      node->wins = wins;
-      node->losses=losses;
-      node->ties=ties;
-      write_node(node, fd, temp->index);
-      printf("%s", "AFTER: ");
-      printPlayer(temp->index, fd);
-      //make input look nicer 
-      printf("> ");
-      return;
-    }else{
-      temp = temp->next;
-    }
-  }
-  printf("ERROR - player does not exist.");
-
-  //make input look nicer 
-  printf("> ");
-  
-  return;
-
-}
-
-void query(Node **head, int fd){
-  int userid;
-  Node *temp = *head;
-
-  //scan in the rest of the line
-  scanf("%d", &userid);
-
-  //find data user needs based on given ID
-  //print that player data
-  while(temp != NULL){
-    if(temp->userid == userid) {
-      printf("QUERY: ");
-      printPlayer(temp->index, fd);
-
-      //make input look nicer 
-      printf("> ");
-
-      return;
-    }else{
-      temp = temp->next;
-    }
-  }
-  //ERROR
-  printf("%s\n", "ERROR - player does not exist.");
-
-  //make input look nicer 
-  printf("> ");
-
-  return;
-}
-
-void printPlayers(Node **head, int fd){
-  Node *temp = *head;
-  while(temp != NULL){
-    printPlayer(temp->index, fd);
-    temp = temp->next;
-  }
-}
-
-void printPlayer(int index, int fd){
-  struct player play;
-  read_player(fd, index, &play);
-  
-  printf("%d, %s, %s, %d, %d, %d \n", play.userid, 
-                                      play.last, 
-                                      play.first, 
-                                      play.wins, 
-                                      play.losses, 
-                                      play.ties);
-  return;
-
-}
 
 void insert(Node **head, Node *newNode){
   Node *temp = *head;
-
 
   if(*head == NULL){
     *head = newNode;
@@ -311,6 +249,81 @@ void insert(Node **head, Node *newNode){
   }
 }
 
+void update(int fd, Node **head){
+  int userid, wins, losses, ties;
+  Node *temp = *head;
+
+  //scan in the rest of the line
+  scanf("%d%d%d%d", &userid, &wins, &losses, &ties);
+   
+  //find data for playerid user entered, change info 
+  //for that node
+  while(temp != NULL){
+    if(temp->userid == userid) {
+      printf("%s", "BEFORE: ");
+      printp(fd, temp->index);
+  
+      Player *node = (Player *)malloc(sizeof(Player));
+      lseek(fd, temp->index*sizeof(Player), 0);
+      readp(fd, temp->index, node);
+ 
+      node->userid = userid;
+      node->wins = wins;
+      node->losses=losses;
+      node->ties=ties;
+
+      writep(fd, temp->index, node);
+
+      printf("%s", "AFTER: ");
+      printp(fd, temp->index);
+      //make input look nicer 
+      printf("> ");
+
+      free(node);
+
+      return;
+    }else{
+      temp = temp->next;
+    }
+  }
+  printf("ERROR - player does not exist.");
+
+  //make input look nicer 
+  printf("> ");
+  return;
+}
+
+void query(int fd, Node **head){
+  int userid;
+  Node *temp = *head;
+
+  //scan in the rest of the line
+  scanf("%d", &userid);
+
+  //find data user needs based on given ID
+  //print that player data
+  while(temp != NULL){
+    if(temp->userid == userid) {
+      printf("QUERY: ");
+      printp(fd, temp->index);
+
+      //make input look nicer 
+      printf("> ");
+
+      return;
+    }else{
+      temp = temp->next;
+    }
+  }
+  //ERROR
+  printf("%s\n", "ERROR - player does not exist.");
+
+  //make input look nicer 
+  printf("> ");
+
+  return;
+}
+
 //free memory starting from head
 void kill(Node **head){
   Node *node = *head; 
@@ -329,6 +342,5 @@ void die(const char *message){
     perror(message);
   else
     printf("ERROR: %s\n", message);
-
   exit(1);
 }
