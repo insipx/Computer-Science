45e7d9969cac599d015e878b1f847f2de387747e
Andrew <aplaza@liquidthink.net>
2018-11-29 21:42:33 -0500

    muxes work as expected

--- 350/ALU/mux.v
+++ 350/ALU/mux.v
@@ -8,22 +8,22 @@
 
 // 1-bit select, 2 1-byte inputs, 1 1-byte output
 module twoOneMux(select, in, out);
-    input[0:1][0:7] in; // 2 byte inputs
+    input[1:0][7:0] in; // 2 byte inputs
     input select;
-    output[0:7] out; // one byte output
+    output[7:0] out; // one byte output
     assign out = in[select];
 endmodule
 
 
 // two bit select, 4 1-byte inputs, 1 1-byte output
 module fourOneMux(select, in, out);
-    input[0:3][0:7] in;
-    input[0:1] select;
-    output[0:7] out;
+    input[3:0][7:0] in;
+    input[1:0] select;
+    output[7:0] out;
 
     wire[1:0][7:0] level0;
-    twoOneMux FIRST(select[0], in[0:1], level0[0]);
-    twoOneMux SECOND(select[0], in[2:3], level0[1]);
+    twoOneMux FIRST(select[0], in[1:0], level0[0]);
+    twoOneMux SECOND(select[0], in[3:2], level0[1]);
 
     twoOneMux OUT(select[1], level0, out);
 
@@ -31,16 +31,16 @@ endmodule
 
 // three bit select, 8 1-byte inputs, 1 1-byte output
 module eightOneMux(select, in, out);
-    input[0:7][0:7] in; // 8 1-byte inputs
-    input[0:2] select; // 3 bit select
-    output[0:7] out; // 1 1-byte output
+    input[7:0][7:0] in; // 8 1-byte inputs
+    input[2:0] select; // 3 bit select
+    output[7:0] out; // 1 1-byte output
 
-    wire[0:1][0:7] first;
+    wire[1:0][7:0] first;
 
     // Level 0
-    fourOneMux LEVEL0_0(select[0:1], in[0:3], first[0]);
-    fourOneMux LEVEL0_1(select[0:1], in[4:7], first[1]);
+    fourOneMux LEVEL0_0(select[1:0], in[3:0], first[0]);
+    fourOneMux LEVEL0_1(select[1:0], in[7:4], first[1]);
 
     // Level 1
-    twoOneMux LEVEL1_0(select[1], first, out);
+    twoOneMux LEVEL1_0(select[2], first, out);
 endmodule
